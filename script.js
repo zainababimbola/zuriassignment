16ad9d01d017e13bc72c0e606b71396f0b03f8f8
zainab <azeezzainab959@gmail.com>
2024-05-16 23:23:37 +0100

    zuri assignment

---
+++ script.js
@@ -0,0 +1,15 @@
+/*let email = document.getElementById("email");
+
+email.addEventListener('click', mistakeOfEmail){
+    
+}
+
+
+function mistakeOfEmail(){
+    if(email === email.replace(/["@"]/g, "")){
+        return "Please enter the correct email";
+    }
+    else{
+        return "error";
+    }
+}*/
